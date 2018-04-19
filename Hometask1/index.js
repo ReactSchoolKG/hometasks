b39a7edc17fe9564ea0073ec653ad20ce1f7a47d
Lorenc326 <batcoh@ukr.net>
2018-04-19 14:44:17 +0300

    Add randomizer

--- Hometask1/index.js
+++ Hometask1/index.js
@@ -1,12 +1,22 @@
 function alertNumbs(e) {
     let target = e.target;
-    if (target.classList.contains('btn')){
-        console.log(target.textContent);
+    while(target != this){  // this == box
+        if (target.classList.contains('btn')){
+            console.log(target.textContent);
+            if(target.classList.contains('red')){
+                target.classList.remove('red');
+                randomizer(this);
+            }
+        }
+        target = target.parentNode;
     }
 }
-function randomizer(){
 
+function randomizer(list){
+    let rand = Math.floor(Math.random() * 10);
+    list.children[rand].classList.add('red');
 }
 
 let box = document.querySelector('div.box');
-box.addEventListener('click', alertNumbs, false);
+box.onclick = alertNumbs;
+randomizer(box);
