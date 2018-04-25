d4b5ad3df7f89644e2c9f37cc51d35a0d5c44d87
bohdanmirchuk <bohdanmirchukslu@ukr.net>
2018-04-25 21:39:20 +0300

    task with star

--- Hometask2/main.js
+++ Hometask2/main.js
@@ -2,8 +2,21 @@ var elements = document.querySelectorAll('div,p,button');
 
 for (var i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", alertTag, true);
-  elements[i].addEventListener("click", alertTag, false);
+  elements[i].addEventListener("click", alertTagUp, false);
 }
 function alertTag(event){
   alert("target = " + event.target.tagName + ", this=" + this.tagName);
+}
+function alertTagUp(event){
+  if (this.tagName == "DIV"){
+    alert("target = " + event.target.tagName + ", this=" + this.tagName);
+    event.stopPropagation();
+  }
+  else{
+    alert("target = " + event.target.tagName + ", this=" + this.tagName);
+  }
+}
+
+document.body.onclick = function(event){
+  alert("Event on body");
 }
